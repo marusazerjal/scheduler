96bfc4f91addd081954f4b135dcf350c3f647835
Marusa Zerjal <marusa.zerjal@gmail.com>
2018-01-14 13:23:07 +1100

    Add files via upload

--- params_simulator.py
+++ params_simulator.py
@@ -17,8 +17,8 @@ data_output_folder = 'data_output_simulator/'
 
 params={'simulate_dates_file': 'simulator/simulator_dates3.dat',
            'MIN_MOON_PHASE_TO_OBSERVE': 0.5, # FunnelWeb gets bright time. This is around 16 nights per month with phase>0.5.
-           'date_start': date(2018, 4, 14),  # start date
-           'date_finish': date(2018, 4, 24),  # end date
+           'date_start': date(2018, 3, 14),  # start date
+           'date_finish': date(2020, 5, 25),  # end date
            'weather_clouds_stats': 'simulator/coonabarabran_cloudy_days_stats.dat',
            'technical_issues_rate': 0.05 # How many times observations are not possible due to the technical issues
         }
